47df0b87b4175d7c374c6b88a55ddfca9b191958
Eduardo Capistran Del Angel <eduardo.capistran@gmail.com>
2017-11-26 09:27:17 -0600

    Adding first view test and some models for migrations.

--- cmdb/models.py
+++ cmdb/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Server(models.Model):
+    SRV_TYPES = (
+        ('P', 'Physical'),
+        ('V', 'Virtual'),
+        ('A', 'Appliance'),
+    )
+
+    SRV_TIERS = (
+        ('AP','Application - Apache, Weblogic, etc'),
+        ('CM','Concurrent Manager'),
+        ('DB','Database'),
+        ('PX','Proxy'),
+        ('NA','Not Applicable'),
+    )
+    hostname = models.CharField(max_length=200)
+    srv_type = models.CharField(max_length=1, choices=SRV_TYPES)
+    tier = models.CharField(max_length=2, choices=SRV_TIERS)
+    
+
+class ServerIP(models.Model):
+    server_id = models.ForeignKey(Server, on_delete=models.CASCADE)
+    ip = models.GenericIPAddressField(protocol='IPv4')
+
+class HRBL_App(models.Model):
+    name = models.CharField(max_length=10)
+    description = models.CharField(max_length=255)
+
+class Server_App(models.Model):
+    server_id = models.ForeignKey(Server)
+    app_id = models.ForeignKey(HRBL_App)
+    notes = models.CharField(max_length=255)
+
+
