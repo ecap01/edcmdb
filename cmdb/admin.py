0bc8408365983248573195479ccc27d579b1994a
Eduardo Capistran Del Angel <eduardo.capistran@gmail.com>
2017-11-27 20:04:03 -0600

    Adding Models to the admin Site

--- cmdb/admin.py
+++ cmdb/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Server, ServerIP, HRBL_App, Server_App
 
-# Register your models here.
+admin.site.register(Server)
+admin.site.register(ServerIP)
+admin.site.register(HRBL_App)
+admin.site.register(Server_App)
+
+
