c045d1b53ae372f3f10a09f362755380470a6655
Eduardo Capistran Del Angel <eduardo.capistran@gmail.com>
2017-11-27 20:35:38 -0600

    Adding strings to Models

--- cmdb/models.py
+++ cmdb/models.py
@@ -17,19 +17,26 @@ class Server(models.Model):
     hostname = models.CharField(max_length=200)
     srv_type = models.CharField(max_length=1, choices=SRV_TYPES)
     tier = models.CharField(max_length=2, choices=SRV_TIERS)
-    
+    def __str__(self):
+        return self.hostname
 
 class ServerIP(models.Model):
     server_id = models.ForeignKey(Server, on_delete=models.CASCADE)
     ip = models.GenericIPAddressField(protocol='IPv4')
+    def __str__(self):
+        return self.ip
 
 class HRBL_App(models.Model):
     name = models.CharField(max_length=10)
     description = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 class Server_App(models.Model):
     server_id = models.ForeignKey(Server)
     app_id = models.ForeignKey(HRBL_App)
     notes = models.CharField(max_length=255)
+    def __str__(self):
+        return "{}-{}".format(self.server_id.hostname,slf.app_id.name)
 
 
